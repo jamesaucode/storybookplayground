8e5517c07b3d8cb445ff5f783d053836b9829aeb
James <jamesaubudi@gmail.com>
2019-06-19 10:12:45 -0400

    eject

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Button = ({ clicked }) => {
+  const [myClicked, setMyClicked] = useState(false);
+  const handleClick = e => {
+    e.preventDefault();
+    setMyClicked(!myClicked);
+  };
   return (
     <button
-      style={{ border: clicked ? "2px solid purple" : "2px solid green" }}
+      onClick={handleClick}
+      style={{ border: myClicked ? "2px solid purple" : "2px solid green" }}
     >
       Test button
     </button>
--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 import { withKnobs, boolean } from "@storybook/addon-knobs/react";
+import { withStorySource } from "@storybook/addon-storysource";
 
 import Button from "./Button";
 
 storiesOf("Basic|Button", module)
   .addDecorator(withKnobs)
+  .addDecorator(withStorySource)
   .add("Not Clicked", () => <Button />)
   .add("Clicked", () => <Button clicked={boolean("clicked", true)} />);
